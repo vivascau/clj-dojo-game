3aad3916a8c5e2505c94b7699d172f39fd8d69c1
vivascau <vlad.ivascau@gmail.com>
2016-01-26 20:53:33 +0000

    Adding game logic

--- clj-dojo/project.clj
+++ clj-dojo/project.clj
@@ -4,8 +4,22 @@
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [compojure "1.4.0"]
-                 [ring/ring-defaults "0.1.5"]]
-  :plugins [[lein-ring "0.9.7"]]
+                 [ring/ring-defaults "0.1.5"]
+                 [org.clojure/clojurescript "1.7.228"]
+                 [reagent "0.6.0-alpha"]]
+  :plugins [[lein-ring "0.9.7"]
+            [lein-cljsbuild "1.1.2"]
+            [lein-figwheel "0.5.0-1"]]
+  :cljsbuild {
+    :builds [{
+        ; The path to the top-level ClojureScript source directory:
+        :source-paths ["src-cljs"]
+        ; The standard ClojureScript compiler options:
+        ; (See the ClojureScript compiler documentation for details.)
+        :compiler {
+          :output-to "resources/public/js/main.js"  ; default: target/cljsbuild-main.js
+          :output-dir "resources/public/js/out"
+          :figwheel true}}]}
   :ring {:handler clj-dojo.handler/app}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
--- clj-dojo/src/clj_dojo/handler.clj
+++ clj-dojo/src/clj_dojo/handler.clj
@@ -1,11 +1,21 @@
 (ns clj-dojo.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [ring.middleware.defaults :refer [wrap-defaults api-defaults]]))
+
+(def guess (atom (inc (rand-int 30))))
+
+(defn post-number [number] (println number) (cond (= @guess number) "correct"
+                                                  (< @guess number) "lower"
+                                                  (> @guess number) "higher"))
+
+(defn get-game [] (println @guess) (str @guess))
 
 (defroutes app-routes
   (GET "/" [] "Hello World")
+  (GET "/game" [] (get-game) )
+  (POST "/guess/:number" [number] (post-number (Integer/parseInt number)))
   (route/not-found "Not Found"))
 
 (def app
-  (wrap-defaults app-routes site-defaults))
+  (wrap-defaults app-routes api-defaults))
